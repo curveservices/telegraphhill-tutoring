c3be748a59786cdaa1d391b8668bb53f0374f953
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-21 19:19:36 +0100

    Add LoadingSpinner component with styles for improved loading experience

---
+++ src/components/loadingSpinner/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.scss";
+
+const LoadingSpinner = () => (
+  <div className="spinner-container">
+    <div className="load-spinner"></div>
+  </div>
+);
+
+export default LoadingSpinner;
