a79e377941aeb41396f15129735211237063c03c
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-21 09:27:39 +0100

    Add Layout component with scroll-to-top functionality and navigation

---
+++ src/layout/main/index.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import Navbar from '../nav';
+import Footer from '../footer';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+
+const Layout = () => {
+    const [showButton, setShowButton] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setShowButton(true)
+            } else showButton(false);
+        });
+    }, []);
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+  return (
+    <>
+          <Navbar />
+          <main className="app">
+              <Outlet />
+          </main>
+          {showButton && (
+              <button
+                  className='back-to-top'
+                  name='back to top'
+                  aria-label='back top top of page'
+                  onClick={scrollToTop}
+              >
+                  <FontAwesomeIcon icon={faArrowUp}/>
+              </button>
+          )}
+          <Footer />
+          <ScrollRestoration />
+    </>
+  )
+}
+
+export default Layout
