428ed95ff805d81f8eb73d222b0f591e6952668f
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-21 21:14:24 +0100

    Add HeroCard component with contact information and styling

---
+++ src/components/heroCard/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faLocation,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import './index.scss';
+
+const HeroCard = () => {
+  return (
+    <div className="hero-card">
+      <div className="title-container">
+        <div className="indent"></div>
+        <h2 className="card-title">Find Me</h2>
+      </div>
+      <p className="card-body">
+        My tutoring can be provided from the comfort of your home, or online
+        using a platform of you choice
+      </p>
+      <div className="contact-container">
+        <div className="contact">
+          <FontAwesomeIcon icon={faPhone} />
+          <p> +44 7980 483384</p>
+        </div>
+        <div className="contact">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <p> vanessaslade@gmail.com</p>
+        </div>
+        <div className="contact">
+          <FontAwesomeIcon icon={faLocation} />
+          <p> Musgrove Road, New Cross, London</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroCard
