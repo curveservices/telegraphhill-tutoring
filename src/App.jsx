dbcd3bd5b1e25caff97c1f18fcb955ec929310aa
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-21 19:18:53 +0100

    Refactor App component to include Suspense and LoadingSpinner for improved loading experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Layout from './layout/main/index.jsx';
+import LoadingSpinner from './components/loadingSpinner'
+import Home from './pages/home'
+
+import './App.scss';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path='/' element={<Layout />}>
-        
+        <Route
+          index
+          element={
+            <Suspense fallback={<LoadingSpinner />} >
+              <Home />
+            </Suspense>
+          }
+        />
       </Route>
     </>
   )
 )
 
 const App = () => {
-
-  return (
-    <div>
-      
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
