7819f4dbb8efe698ddf051ed8f079c7d5a5375b2
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-21 09:57:17 +0100

    Add routing for the Layout component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import Layout from './layout/main/index.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+      <Route path='/' element={<Layout />}>
+        
+      </Route>
     </>
   )
 )
