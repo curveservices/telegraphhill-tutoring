94939599b59770134589586adce881cfb6db7ae5
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-22 07:52:41 +0100

    Enhance DoubleImage component with hover effects and improved structure

--- src/components/doubleImage/index.jsx
+++ src/components/doubleImage/index.jsx
@@ -5,50 +5,56 @@ import { NavLink } from 'react-router-dom';
 const DoubleImage = (props) => {
   return (
     <>
-    <div className="image-container">
-      <div className="square-shape"></div>
-      <img
-        src={props.img1}
-        alt="Private 11 plus tutor in London"
-        className="image"
-        style={{ marginBottom: "60px" }}
-      />
-      <img
-        src={props.img2}
-        alt="Private tutoring lessons in south east London"
-        className="image"
-        style={{ marginTop: "60px" }}
-      />
-      <div className="circle-shape"></div>
+      <div className="image-container">
+        <div className="square-shape"></div>
+        <img
+          src={props.img1}
+          alt="Private 11 plus tutor in London"
+          className="image"
+          style={{ marginBottom: "60px" }}
+        />
+        <img
+          src={props.img2}
+          alt="Private tutoring lessons in south east London"
+          className="image"
+          style={{ marginTop: "60px" }}
+        />
+        <div className="circle-shape"></div>
         <div className="subject-container">
-          <div className="card ks1">
-            <div className="card-title">KS1</div>
-            <NavLink to='tuition'>Tuition</NavLink>
-          </div>
-          <div className="card ks2">
-            <div className="card-title">KS2</div>
-            <NavLink to='tuition'>Tuition</NavLink>
-          </div>
-          <div className="card 11-plus">
-            <div className="card-title">11 +</div>
-            <NavLink to='resources'>Resources</NavLink>
-          </div>
+          <NavLink to="tuition">
+            <div className="card ks1">
+              <div className="card-title">KS1</div>
+              <div className="text">Tuition</div>
+            </div>
+          </NavLink>
+          <NavLink to="tuition">
+            <div className="card ks2">
+              <div className="card-title">KS2</div>
+              <div className="text">Tuition</div>
+            </div>
+          </NavLink>
+          <NavLink to="resources">
+            <div className="card 11-plus">
+              <div className="card-title">11 +</div>
+              <div className="text">Resources</div>
+            </div>
+          </NavLink>
+        </div>
       </div>
-    </div>
-    <div className="text-box">
-            <div className="subtitle">
-              <div className="indent"></div>
+      <div className="text-box">
+        <div className="subtitle">
+          <div className="indent"></div>
           <div>{props.subtitle}</div>
-            </div>
+        </div>
         <h2 className="title">{props.title}</h2>
-            <div className="hero-p">
-              <div className="text-box-bottom">
-                <p>{props.p1}</p>
-                <p>{props.p2}</p>
-              </div>
-            </div>
+        <div className="hero-p">
+          <div className="text-box-bottom">
+            <p>{props.p1}</p>
+            <p>{props.p2}</p>
+          </div>
+        </div>
       </div>
-      </>
+    </>
   );
 }
 
