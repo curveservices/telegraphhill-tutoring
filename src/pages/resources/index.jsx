9ea7923df8cf615e6968ba2a9c94d049ff2d5db2
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-28 10:30:19 +0100

    Add Resources page component and styling; create initial layout structure

---
+++ src/pages/resources/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.scss';
+
+const Resources = () => {
+  return (
+    <div className='resources-page'>
+      <section className="hero">
+        <div className="inner-hero">
+          
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Resources
