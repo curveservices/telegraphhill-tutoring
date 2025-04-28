ce0f01a757294dca5311b93b3f5d4200ea4eb7ca
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-28 10:11:30 +0100

    Refactor HeroCard component to accept props for dynamic content; adjust styles for improved layout and responsiveness

--- src/components/heroCard/index.jsx
+++ src/components/heroCard/index.jsx
@@ -1,42 +1,61 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEnvelope,
   faLocation,
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
-import Socials from '../socials';
-import './index.scss';
+import Socials from "../socials";
+import "./index.scss";
 
-const HeroCard = () => {
+const HeroCard = (props) => {
+  const {
+    title,
+    cardtitle1,
+    cardtitle2,
+    p1,
+    p2,
+    showContact = true,
+    divLine = true,
+  } = props;
   return (
     <div className="hero-card">
       <div className="title-container">
         <div className="indent"></div>
-        <h2 className="card-title">Find Me</h2>
+        <h2>{title}</h2>
       </div>
-      <p className="card-body">
-        My tutoring can be provided from the comfort of your home, or online
-        using a platform of you choice
-      </p>
-      <div className="div-line"></div>
-      <div className="contact-container">
-        <div className="contact">
-          <FontAwesomeIcon icon={faPhone} />
-          <p> +44 7980 483384</p>
-        </div>
-        <div className="contact">
-          <FontAwesomeIcon icon={faEnvelope} />
-          <p> vanessaslade@gmail.com</p>
-        </div>
-        <div className="contact">
-          <FontAwesomeIcon icon={faLocation} />
-          <p> Musgrove Road, New Cross, London</p>
-        </div>
-        <Socials />
-      </div>
+
+      {cardtitle1 && <div className="card-title">{cardtitle1}</div>}
+
+      <p className="card-body">{p1}</p>
+      { divLine && <div className="div-line"></div>}
+
+      {cardtitle2 && <div className="card-title">{cardtitle2}</div>}
+
+      {p2 && <p className="card-body">{p2}</p>}
+      {showContact && (
+        <>
+          <div className="div-line"></div>
+
+          <div className="contact-container">
+            <div className="contact">
+              <FontAwesomeIcon icon={faPhone} />
+              <p>+44 7980 483384</p>
+            </div>
+            <div className="contact">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <p>vanessaslade@gmail.com</p>
+            </div>
+            <div className="contact">
+              <FontAwesomeIcon icon={faLocation} />
+              <p>Musgrove Road, New Cross, London</p>
+            </div>
+            <Socials />
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
 
-export default HeroCard
+export default HeroCard;
