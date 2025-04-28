60c2ffa9deead5f600fe2387f77c931968ee53c3
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-28 13:52:44 +0100

    Fix button component structure and styling; correct fontSize property and remove unnecessary Link wrapper

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -6,28 +6,42 @@ const Button = (props) => {
   const buttonStyles = {
     background: props.background,
     color: props.color,
-    fonSize: props.fontSize + "px",
-    boxShadow: props.boxShadow,
+    fontSize: props.fontSize + "px",
   };
-  return (
-    <Link
-      to={props.link}
-      type={props.type}
-      target={props.target}
-      role="button"
-      tabIndex={0}
-      aria-label={`Link to ${props.text}`}
-    >
-      <button
-        className="flat-btn"
-        style={buttonStyles}
-        onClick={props.onClick}
-        type={props.type}
-        value={props.value}
+
+  if (props.link) {
+    return (
+      <Link
+        to={props.link}
+        target={props.target}
+        role="button"
+        tabIndex={0}
+        aria-label={`Link to ${props.text}`}
       >
-        {props.text}
-      </button>
-    </Link>
+        <button
+          className="flat-btn"
+          style={buttonStyles}
+          onClick={props.onClick}
+          type={props.type}
+          value={props.value}
+        >
+          {props.text}
+        </button>
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      className="flat-btn"
+      style={buttonStyles}
+      onClick={props.onClick}
+      type={props.type}
+      value={props.value}
+      aria-label={props.text}
+    >
+      {props.text}
+    </button>
   );
 };
 
