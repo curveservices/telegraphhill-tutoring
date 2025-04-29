f1f28d533cc08c88a16bbde57cff6f356d98138d
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-29 08:02:47 +0100

    feat: remove dropdown menu from Navbar for cleaner navigation

--- src/layout/nav/index.jsx
+++ src/layout/nav/index.jsx
@@ -80,38 +80,6 @@ const Navbar = () => {
                 <div className="text">Resources</div>
               </div>
             </NavLink>
-            {showDropdown && (
-              <div className="dropdown-menu">
-                <NavLink
-                  to="/11-plus-english-papers"
-                  onClick={() => setShowDropdown(false) & setShowNav(false)}
-                  className="dropdown-item"
-                >
-                  11 Plus English Solved Papers
-                </NavLink>
-                <NavLink
-                  to="/11-plus-maths-papers"
-                  onClick={() => setShowDropdown(false) & setShowNav(false)}
-                  className="dropdown-item"
-                >
-                  11 Plus Maths Solved Papers
-                </NavLink>
-                <NavLink
-                  to="/11-plus-verbal-reasoning-papers"
-                  onClick={() => setShowDropdown(false) & setShowNav(false)}
-                  className="dropdown-item"
-                >
-                  11 Plus Verbal Reasoning Papers
-                </NavLink>
-                <NavLink
-                  to="/11-plus-non-verbal-reasoning-papers"
-                  onClick={() => setShowDropdown(false) & setShowNav(false)}
-                  className="dropdown-item"
-                >
-                  11 Plus Non-verbal Reasoning Papers
-                </NavLink>
-              </div>
-            )}
           </div>
           <Button
             text="Contact Me"
